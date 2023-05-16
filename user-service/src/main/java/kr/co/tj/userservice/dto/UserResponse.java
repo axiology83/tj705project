148a3d1175ea8372a69a222e465bb0611c9952f0
jongbong <snulifeo2@naver.com>
2023-05-16 15:57:36 +0900

    시큐리티/멤버 로긴 등

--- user-service/src/main/java/kr/co/tj/userservice/dto/UserResponse.java
+++ user-service/src/main/java/kr/co/tj/userservice/dto/UserResponse.java
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
